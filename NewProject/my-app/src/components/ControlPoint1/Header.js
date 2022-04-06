e0b64b59bf7aba3d5f24dd9862b062d59f0a6c2c
BetlBit <inthebetmen@mail.ru>
2022-04-06 23:32:11 +0300

    Practicle

--- NewProject/my-app/src/components/ControlPoint1/Header.js
+++ NewProject/my-app/src/components/ControlPoint1/Header.js
@@ -1,14 +1,39 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Logo from "./Logo"
 import Nav from "./Nav"
 import Register from "./Register"
 
+function clc(){
+	window.location = "/Login"}
+
 function Header(){
+	const [che, setChecked] = useState(localStorage.getItem("theme") === "dark" ? true : false);
+	useEffect(() => {
+		document.getElementsByTagName("Body")[0].setAttribute("data-theme", localStorage.getItem("theme"));
+	}, [che]);
+
+	function Change() {
+		if (che === false) {
+			localStorage.setItem("theme", "dark");
+			
+			setChecked(true);
+		} else {
+			localStorage.setItem("theme", "light");
+
+			setChecked(false);
+		}
+	}
+
 	return (
 		<div id="Header">
 			<Logo/>
 			<Nav/>
 			<Register/>
+			<div className='5ff'>
+				<label><span>Сменить тему</span>
+					<input type="checkbox"	defaultChecked={che}	onChange={() => Change()}/>
+				</label>
+				</div>
 		</div>
 	)
 }
